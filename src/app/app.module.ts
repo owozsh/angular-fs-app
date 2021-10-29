a3e9a776f554318b2f186d694696fd8e6f87a982
owozsh <owozsh@posteo.net>
2021-10-29 14:29:22 -0300

    add buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -14,6 +16,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
