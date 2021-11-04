8af9e80c72cff869121cdf1ba5b8f4c091e65303
owozsh <owozsh@posteo.net>
2021-11-04 11:29:44 -0300

    polish edit feature

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { StringifyOptions } from 'querystring';
 
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -37,17 +38,25 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
-  getPost(id: string) {
-    return { ...this.posts.find((post) => post.id === id) };
-  }
-
   updatePost(id: string, title: string, content: string) {
     const post: Post = { id: id, title: title, content: content };
     this.http
       .put(`http://localhost:3000/api/posts/${id}`, post)
       .subscribe((response) => {
-        console.log(response);
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(
+          (post) => post.id === post.id
+        );
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
       });
+  }
+
+  getPost(id: string) {
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      `http://localhost:3000/api/posts/${id}`
+    );
   }
 
   addPost(title: string, content: string) {
